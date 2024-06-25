4c894ea9112170ceec2502afbf10e0771f06530c
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-26 01:35:45 +0600

    create-redux-sotre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 function App() {
   return (
     <>
-      <h1>React with Redux </h1>
+      <div className="items-center w-full h-screen flex justify-center ">
+        <div className="flex border border-purple-300 bg-slate-200 p-6 gap-3">
+          <button className="px-3 py-2 bg-green-500 text-xl font-semibold text-white rounded-md">
+            Increment
+          </button>
+          <h1 className="text-3xl mx-`19">0</h1>
+          <button className="px-3 py-2 bg-red-500 rounded-md text-xl font-semibold text-white">
+            Dcrement
+          </button>
+        </div>
+      </div>
     </>
   );
 }
