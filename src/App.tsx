3aca137d860c9706fffae9d6f94d3c35f4372288
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-27 09:52:51 +0600

    update-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/fratures/counterSlice";
+
 function App() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <>
       <div className="items-center w-full h-screen flex justify-center ">
         <div className="flex border border-purple-300 bg-slate-200 p-6 gap-3">
-          <button className="px-3 py-2 bg-green-500 text-xl font-semibold text-white rounded-md">
+          <button
+            onClick={() => dispatch(increment())}
+            className="px-3 py-2 bg-green-500 text-xl font-semibold text-white rounded-md"
+          >
             Increment
           </button>
-          <h1 className="text-3xl mx-`19">0</h1>
-          <button className="px-3 py-2 bg-red-500 rounded-md text-xl font-semibold text-white">
+          <h1 className="text-3xl mx-`19">{count}</h1>
+          <button
+            onClick={() => dispatch(decrement())}
+            className="px-3 py-2 bg-red-500 rounded-md text-xl font-semibold text-white"
+          >
             Dcrement
           </button>
         </div>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./fratures/counterSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
+
+
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
